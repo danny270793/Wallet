7ec776e9248144edd0b4bff2135536293495c981
Danny Vaca <danny270793@icloud.com>
2026-04-28 12:12:18 -0500

    refactor(accounts): use ShellScaffold with local app bar

--- flutter/lib/pages/accounts_page.dart
+++ flutter/lib/pages/accounts_page.dart
@@ -5,6 +5,7 @@ import '../core/di/injection.dart';
 import '../features/accounts/domain/entities/account_entity.dart';
 import '../features/accounts/presentation/cubit/accounts_cubit.dart';
 import '../features/accounts/presentation/cubit/accounts_state.dart';
+import '../widgets/shell_scaffold.dart';
 import '../widgets/swipeable_list_tile.dart';
 
 class AccountsPage extends StatelessWidget {
@@ -35,18 +36,21 @@ class _AccountsView extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return Stack(
-          children: [
-            _body(context, state, l10n),
-            Positioned(
-              right: 16,
-              bottom: 16,
-              child: FloatingActionButton(
-                onPressed: () => _showNewAccountBottomSheet(context, l10n),
-                child: const Icon(Icons.add),
+        return ShellScaffold(
+          title: l10n.accounts,
+          body: Stack(
+            children: [
+              _body(context, state, l10n),
+              Positioned(
+                right: 16,
+                bottom: 16,
+                child: FloatingActionButton(
+                  onPressed: () => _showNewAccountBottomSheet(context, l10n),
+                  child: const Icon(Icons.add),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
