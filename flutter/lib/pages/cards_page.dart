ee4289dc801c454f52fe24f19eb5ed4787dba17b
Danny Vaca <danny270793@icloud.com>
2026-04-28 12:12:18 -0500

    refactor(cards): use ShellScaffold with local app bar

--- flutter/lib/pages/cards_page.dart
+++ flutter/lib/pages/cards_page.dart
@@ -5,6 +5,7 @@ import '../core/di/injection.dart';
 import '../features/cards/domain/entities/card_entity.dart';
 import '../features/cards/presentation/cubit/cards_cubit.dart';
 import '../features/cards/presentation/cubit/cards_state.dart';
+import '../widgets/shell_scaffold.dart';
 import '../widgets/swipeable_list_tile.dart';
 
 class CardsPage extends StatelessWidget {
@@ -35,18 +36,21 @@ class _CardsView extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return Stack(
-          children: [
-            _body(context, state, l10n),
-            Positioned(
-              right: 16,
-              bottom: 16,
-              child: FloatingActionButton(
-                onPressed: () => _showCardDialog(context, l10n),
-                child: const Icon(Icons.add),
+        return ShellScaffold(
+          title: l10n.cards,
+          body: Stack(
+            children: [
+              _body(context, state, l10n),
+              Positioned(
+                right: 16,
+                bottom: 16,
+                child: FloatingActionButton(
+                  onPressed: () => _showCardDialog(context, l10n),
+                  child: const Icon(Icons.add),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
