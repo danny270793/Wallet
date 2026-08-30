0784eaed20421d515adb692b0dc007cdefb34e5e
Danny Vaca <danny270793@icloud.com>
2026-08-30 11:56:41 -0500

    feat(tags): thread hidden through TagsRepository.updateTag

--- flutter/lib/features/tags/domain/repositories/tags_repository.dart
+++ flutter/lib/features/tags/domain/repositories/tags_repository.dart
@@ -4,6 +4,11 @@ import '../entities/tag_entity.dart';
 abstract class TagsRepository {
   Future<OfflineServedBundle<List<TagEntity>>> getTags();
   Future<TagEntity> createTag({required String name, String? description});
-  Future<TagEntity> updateTag({required String id, required String name, String? description});
+  Future<TagEntity> updateTag({
+    required String id,
+    required String name,
+    String? description,
+    bool hidden = false,
+  });
   Future<void> deleteTag({required String id});
 }
