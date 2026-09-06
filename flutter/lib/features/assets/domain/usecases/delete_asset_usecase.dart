8549e8943786bb01e66adac052b70c02b0ff9160
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:32:19 -0500

    chore: remove delete_asset_usecase.dart

--- flutter/lib/features/assets/domain/usecases/delete_asset_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/assets_repository.dart';
-
-class DeleteAssetUsecase {
-  final AssetsRepository _repository;
-  const DeleteAssetUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteAsset(id: id);
-}
