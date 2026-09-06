92026e5c2d00b2cdfbf53aa9f39d724164fcf7db
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:43:26 -0500

    feat: add lib/features/assets/domain/usecases/get_assets_usecase.dart

---
+++ lib/features/assets/domain/usecases/get_assets_usecase.dart
@@ -0,0 +1,10 @@
+import '../../../../core/offline/offline_served_bundle.dart';
+import '../entities/asset_entity.dart';
+import '../repositories/assets_repository.dart';
+
+class GetAssetsUsecase {
+  final AssetsRepository _repository;
+  const GetAssetsUsecase(this._repository);
+
+  Future<OfflineServedBundle<List<AssetEntity>>> call() => _repository.getAssets();
+}
