58fd38f49232f5a3d933b84cc7a9696e3b1a8fbd
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:32:03 -0500

    chore: remove accounts_repository.dart

--- flutter/lib/features/accounts/domain/repositories/accounts_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../../core/offline/offline_served_bundle.dart';
-import '../entities/account_entity.dart';
-
-abstract class AccountsRepository {
-  Future<OfflineServedBundle<List<AccountEntity>>> getAccounts();
-  Future<AccountEntity> createAccount({required String name, String? description});
-  Future<AccountEntity> updateAccount({required String id, required String name, String? description});
-  Future<void> deleteAccount({required String id});
-}
