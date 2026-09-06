2fa52eab20038da316ed8bb61efc3197a098e6ef
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:43:21 -0500

    feat: add lib/features/assets/domain/repositories/assets_repository.dart

---
+++ lib/features/assets/domain/repositories/assets_repository.dart
@@ -0,0 +1,26 @@
+import '../../../../core/offline/offline_served_bundle.dart';
+import '../entities/asset_entity.dart';
+
+abstract class AssetsRepository {
+  Future<OfflineServedBundle<List<AssetEntity>>> getAssets();
+  Future<AssetEntity> createAsset({
+    required String name,
+    required String provider,
+    required double value,
+    required DateTime boughtAt,
+    DateTime? endedAt,
+    double? soldValue,
+  });
+
+  Future<AssetEntity> updateAsset({
+    required String id,
+    required String name,
+    required String provider,
+    required double value,
+    required DateTime boughtAt,
+    DateTime? endedAt,
+    double? soldValue,
+  });
+
+  Future<void> deleteAsset({required String id});
+}
