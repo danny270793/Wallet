4e9828e98bc72d7b463c9bc2e18178088ff27f12
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:32:06 -0500

    chore: remove delete_account_usecase.dart

--- flutter/lib/features/accounts/domain/usecases/delete_account_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/accounts_repository.dart';
-
-class DeleteAccountUsecase {
-  final AccountsRepository _repository;
-  const DeleteAccountUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteAccount(id: id);
-}
