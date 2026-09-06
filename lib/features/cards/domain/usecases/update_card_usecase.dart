121ecd64042d53d42673c8952686d482549f3914
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:43:57 -0500

    feat: add lib/features/cards/domain/usecases/update_card_usecase.dart

---
+++ lib/features/cards/domain/usecases/update_card_usecase.dart
@@ -0,0 +1,22 @@
+import '../entities/card_entity.dart';
+import '../repositories/cards_repository.dart';
+
+class UpdateCardUsecase {
+  final CardsRepository _repository;
+  const UpdateCardUsecase(this._repository);
+
+  Future<CardEntity> call({
+    required String id,
+    required String name,
+    String? description,
+    required int cutDay,
+    required int payDay,
+  }) =>
+      _repository.updateCard(
+        id: id,
+        name: name,
+        description: description,
+        cutDay: cutDay,
+        payDay: payDay,
+      );
+}
