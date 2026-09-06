b041a997f376e43aa2c4403e8491c49a7ac7ead7
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:44:05 -0500

    feat: add categories_repository.dart

---
+++ lib/features/categories/domain/repositories/categories_repository.dart
@@ -0,0 +1,9 @@
+import '../../../../core/offline/offline_served_bundle.dart';
+import '../entities/category_entity.dart';
+
+abstract class CategoriesRepository {
+  Future<OfflineServedBundle<List<CategoryEntity>>> getCategories();
+  Future<CategoryEntity> createCategory({required String name, String? description});
+  Future<CategoryEntity> updateCategory({required String id, required String name, String? description});
+  Future<void> deleteCategory({required String id});
+}
