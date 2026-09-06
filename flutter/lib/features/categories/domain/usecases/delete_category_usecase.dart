c4a193c7a11267dea02da80d832fff733df9d306
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:32:58 -0500

    chore: remove delete_category_usecase.dart

--- flutter/lib/features/categories/domain/usecases/delete_category_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/categories_repository.dart';
-
-class DeleteCategoryUsecase {
-  final CategoriesRepository _repository;
-  const DeleteCategoryUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteCategory(id: id);
-}
