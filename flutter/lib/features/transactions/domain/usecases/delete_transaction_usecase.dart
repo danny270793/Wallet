3deb93d17144ef67ed1289701addc72e68b09f2b
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:37:16 -0500

    chore: remove delete_transaction_usecase.dart

--- flutter/lib/features/transactions/domain/usecases/delete_transaction_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/transactions_repository.dart';
-
-class DeleteTransactionUsecase {
-  final TransactionsRepository _repository;
-  const DeleteTransactionUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteTransaction(id: id);
-}
