803e226e61454b50c18dc654d80ea689a8f14c9b
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:32:47 -0500

    chore: remove delete_card_usecase.dart

--- flutter/lib/features/cards/domain/usecases/delete_card_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/cards_repository.dart';
-
-class DeleteCardUsecase {
-  final CardsRepository _repository;
-  const DeleteCardUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteCard(id: id);
-}
