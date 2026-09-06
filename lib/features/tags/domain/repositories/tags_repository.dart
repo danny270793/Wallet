04bbc69a864d2b9d0081cd44d9a42a7021d25735
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:44:19 -0500

    feat: add lib/features/tags/domain/repositories/tags_repository.dart

---
+++ lib/features/tags/domain/repositories/tags_repository.dart
@@ -0,0 +1,14 @@
+import '../../../../core/offline/offline_served_bundle.dart';
+import '../entities/tag_entity.dart';
+
+abstract class TagsRepository {
+  Future<OfflineServedBundle<List<TagEntity>>> getTags();
+  Future<TagEntity> createTag({required String name, String? description});
+  Future<TagEntity> updateTag({
+    required String id,
+    required String name,
+    String? description,
+    bool hidden = false,
+  });
+  Future<void> deleteTag({required String id});
+}
