fd2b81d56896fa7a3761107c52c42a6a05d7c590
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:33:13 -0500

    chore: remove delete_tag_usecase.dart

--- flutter/lib/features/tags/domain/usecases/delete_tag_usecase.dart
+++
@@ -1,8 +0,0 @@
-import '../repositories/tags_repository.dart';
-
-class DeleteTagUsecase {
-  final TagsRepository _repository;
-  const DeleteTagUsecase(this._repository);
-
-  Future<void> call({required String id}) => _repository.deleteTag(id: id);
-}
