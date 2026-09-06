4bf96096c4ff8b23d38350b6230354f0e2bddcab
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:44:18 -0500

    feat: add lib/features/tags/domain/entities/tag_entity.dart

---
+++ lib/features/tags/domain/entities/tag_entity.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+class TagEntity extends Equatable {
+  final String id;
+  final String userId;
+  final String name;
+  final String? description;
+  final bool hidden;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const TagEntity({
+    required this.id,
+    required this.userId,
+    required this.name,
+    this.description,
+    this.hidden = false,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory TagEntity.fromJson(Map<String, dynamic> json) => TagEntity(
+    id: json['id'] as String,
+    userId: json['userId'] as String,
+    name: json['name'] as String,
+    description: json['description'] as String?,
+    hidden: json['hidden'] as bool? ?? false,
+    createdAt: DateTime.parse(json['createdAt'] as String),
+    updatedAt: DateTime.parse(json['updatedAt'] as String),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'userId': userId,
+    'name': name,
+    'description': description,
+    'hidden': hidden,
+    'createdAt': createdAt.toUtc().toIso8601String(),
+    'updatedAt': updatedAt.toUtc().toIso8601String(),
+  };
+
+  @override
+  List<Object?> get props => [id, userId, name, description, hidden, createdAt, updatedAt];
+}
