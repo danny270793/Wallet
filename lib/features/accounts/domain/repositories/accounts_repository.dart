67473e0ee503227b2d122d139a29cdd7d908f382
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:43:08 -0500

    feat: add accounts_repository.dart

---
+++ lib/features/accounts/domain/repositories/accounts_repository.dart
@@ -0,0 +1,9 @@
+import '../../../../core/offline/offline_served_bundle.dart';
+import '../entities/account_entity.dart';
+
+abstract class AccountsRepository {
+  Future<OfflineServedBundle<List<AccountEntity>>> getAccounts();
+  Future<AccountEntity> createAccount({required String name, String? description});
+  Future<AccountEntity> updateAccount({required String id, required String name, String? description});
+  Future<void> deleteAccount({required String id});
+}
